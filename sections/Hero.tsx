b73b184240af4f9eb1d09fa19d7fc54d23327529
Saad Shaikh <shaikhsaad497@gmail.com>
2024-07-14 16:08:48 +0530

    nothing

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -1,20 +1,35 @@
-interface Props {
-  /**
-  * @description The description of name.
-  */
-  name?: string;
-}
+// interface Props {
+//   /**
+//   * @description The description of name.
+//   */
+//   name?: string;
+// }
 
-export default function Section({ name = "Capy" }: Props) {
-  return (
+export default function Hero(
+    { heading }: { heading: string },
+    // { descriptor1 }: { descriptor1?: string },
+    // { descriptor2 }: { descriptor2?: string },
+) {
+    return (
         <section class="bg-[#FCF8F1] bg-opacity-30 py-10 sm:py-16 lg:py-24">
             <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
                 <div class="grid items-center grid-cols-1 gap-12 lg:grid-cols-2">
                     <div>
-                        <p class="text-base font-semibold tracking-wider text-blue-600 uppercase">A social media for learners</p>
-                        <h1 class="mt-4 text-4xl font-bold text-black lg:mt-8 sm:text-6xl xl:text-8xl">Connect & learn from the experts</h1>
-                        <p class="mt-4 text-base text-black lg:mt-8 sm:text-xl">Grow your career fast with right mentor.</p>
-
+                        {
+                            /* <p class="text-base font-semibold tracking-wider text-blue-600 uppercase">
+                            {descriptor1}
+                        </p> */
+                        }
+                        <h1 class="mt-4 text-4xl font-bold text-black lg:mt-8 sm:text-6xl xl:text-8xl">
+                            {heading}
+                        </h1>
+                        {
+                            /* <p class="mt-4 text-base text-black lg:mt-8 sm:text-xl">
+                            {descriptor2}
+                        </p> */
+                        }
+                        {
+                            /*
                         <a href="#" title="" class="inline-flex items-center px-6 py-4 mt-8 font-semibold text-black transition-all duration-200 bg-yellow-300 rounded-full lg:mt-16 hover:bg-yellow-400 focus:bg-yellow-400" role="button">
                             Join for free
                             <svg class="w-6 h-6 ml-8 -mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -22,14 +37,19 @@ export default function Section({ name = "Capy" }: Props) {
                             </svg>
                         </a>
 
-                        <p class="mt-5 text-gray-600">Already joined us? <a href="#" title="" class="text-black transition-all duration-200 hover:underline">Log in</a></p>
+                        <p class="mt-5 text-gray-600">Already joined us? <a href="#" title="" class="text-black transition-all duration-200 hover:underline">Log in</a></p> */
+                        }
                     </div>
 
                     <div>
-                        <img class="w-full" src="https://cdn.rareblocks.xyz/collection/celebration/images/hero/1/hero-img.png" alt="" />
+                        <img
+                            class="w-full"
+                            src="https://cdn.rareblocks.xyz/collection/celebration/images/hero/1/hero-img.png"
+                            alt=""
+                        />
                     </div>
                 </div>
             </div>
         </section>
-  )
+    );
 }
